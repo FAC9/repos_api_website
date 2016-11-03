360ae1baf59763881b5b15f8bc53782de9bf8a84
emily <limeyb7@gmail.com>
2016-11-03 11:41:50 +0000

    add second api call which generates title of random movie

--- js/app.js
+++ js/app.js
@@ -1,25 +1,79 @@
 var randomPage = 0;
+//var xhr = new XMLHttpRequest;
+//xhr.addEventListener("load", function() {
+var apikey = config.apikey;
+var url = "https://api.themoviedb.org/3/discover/movie?api_key=" + apikey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1"
 
-var xhr = new XMLHttpRequest;
-xhr.addEventListener("load", function() {
-    var apiResponse = JSON.parse(xhr.responseText);
+function makeRequest(url, cb) {
+    httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          cb(null, httpRequest.responseText);
+        } else {
+          cb('There was a problem with the request.');
+        }
+      }
+    };
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
 
+function getNewMovieUrl(_, cb) {
+  makeRequest(url, function(err, res){
+    // cb(null, moviesList[random(res.movies.length)])
+    var apiResponse = JSON.parse(res);
     var total_pages = apiResponse.total_pages;
-    randomPage = Math.floor(Math.random() * total_pages) + 1;
-
-
-
-console.log(apiResponse);
-  return apiResponse;
-});
-
-function getResults(url) {
-  xhr.open("GET", url, true);
-  xhr.send();
+    randomPage = Math.floor(Math.random() * (total_pages > 1000 ? 1000 : total_pages)) + 1;
+    url += "&page=" + randomPage;
+    return cb(null, url);
+  })
 }
 
-var apikey = config.apikey;
-var url = "https://api.themoviedb.org/3/discover/movie?api_key=" + apikey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true"
-getResults(url);
-getResults(url + "&page=" + randomPage);
-console.log(randomPage);
+function getRandomMovie(url, cb) {
+    makeRequest(url, function(err, res){
+      var apiResponse = JSON.parse(res);
+      var moviesOnPage = apiResponse.results.length;
+      console.log(moviesOnPage);
+      var randomMovieIndex = Math.floor(Math.random() * moviesOnPage) + 1;
+      console.log(randomMovieIndex);
+      console.log(err, apiResponse.results[randomMovieIndex - 1].title);
+    });
+}
+
+var waterfall = function(arg, tasks, cb) {
+  console.log('in waterfall');
+  console.log(arg);
+  var next = tasks[0]
+  var tail = tasks.slice(1)
+  if (typeof next !== 'undefined') {
+    console.log("in the if")
+    next(arg, function(error, result) {
+      console.log(arg);
+      console.log('calling next');
+      if (error) {
+        cb(error)
+        return ;
+      }
+      waterfall(result, tail, cb)
+    })
+    return ;
+  }
+  console.log('thinking type is undefined');
+  return cb(null, arg)
+}
+
+waterfall(url, [
+  getNewMovieUrl,
+  getRandomMovie,
+  getNewMovieUrl
+], function(error, result) {
+  console.log('Test 1');
+  if (error) {
+    throw new Error('test failed with error: ' + error)
+  }
+});
